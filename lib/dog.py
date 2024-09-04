7b767e34652a6b3b04502bb3807f42b7451e81b1
terenceouma <doros.devs@gmail.com>
2024-09-04 12:33:03 +0300

    built class in person and dog file

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        # The __init__ method is called when an object is instantiated.
+        # It initializes the instance with the provided name and breed.
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        # The __init__ method is called when an object is instantiated.
+        # It initializes the instance with the provided name.
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
